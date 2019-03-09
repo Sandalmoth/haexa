148f229cb01ca34646b36d922db0f80ec0a4f798
Jonathan <j@localhost.localdomain>
2019-03-09 21:15:18 +0100

    add arithmetic operators

--- include/haexa.h
+++ include/haexa.h
@@ -3,6 +3,7 @@
 
 
 #include <array>
+#include <cassert>
 #include <cmath>
 #include <iostream>
 
@@ -26,6 +27,7 @@ namespace haexa {
       int iz = std::round(z);
       int sum = ix + iy + iz;
       if (sum != 0) {
+        // TODO change implementation so that it works even if some diffs are equal
         double dx = fabs(x - static_cast<double>(ix));
         double dy = fabs(y - static_cast<double>(iy));
         double dz = fabs(z - static_cast<double>(iz));
@@ -37,6 +39,7 @@ namespace haexa {
           iz = -(ix + iy);
         }
       }
+      assert(ix + iy + iz == 0);
       return Hex<int>(ix, iy, iz);
     }
 
@@ -46,12 +49,44 @@ namespace haexa {
       return x + y + z == 0;
     }
 
+
+    // Operator overloading
+    template <typename TOther>
+    Hex<T> operator+(const Hex<TOther> &other) {
+      return Hex<T>(x + other.x, y + other.y, z + other.z);
+    }
+
+    template <typename TOther>
+    Hex<T> operator-(const Hex<TOther> &other) {
+      return Hex<T>(x - other.x, y - other.y, z - other.z);
+    }
+
+    template <typename T2, typename TScalar>
+    friend Hex<TScalar> operator*(const Hex<T2> &, TScalar);
+    template <typename T2, typename TScalar>
+    friend Hex<TScalar> operator*(TScalar, const Hex<T2> &);
+
+    template <typename TScalar>
+    Hex<TScalar> operator/(TScalar a) {
+      return Hex<TScalar>(x/a, y/a, z/a);
+    }
+
+
+    // Default printing
     template <typename T2>
     friend std::ostream &operator<<(std::ostream &, Hex<T2>);
 
   private:
     T x, y, z;
   };
+
+
+  template <typename T, typename TScalar>
+  Hex<TScalar> operator*(const Hex<T> &hex, TScalar a) {
+    return Hex<TScalar>(hex.x*a, hex.y*a, hex.z*a);
+  }
+  template <typename T, typename TScalar>
+  Hex<TScalar> operator*(TScalar a, const Hex<T> &hex) { return hex*a; }
 
 
   template <typename T>
--- test/scratch.cpp
+++ test/scratch.cpp
@@ -26,4 +26,50 @@ int main() {
     cout << hex << ' ' << hex.round() << '\t' << (hex.round().valid() ? "ok" : "invalid") << endl;
   }
 
+  cout << "\nHex addition" << endl;
+  uniform_int_distribution<int> d_sum(-3, 3);
+  for (int i = 0; i < 5; ++i) {
+    int a1 = d_sum(rng);
+    int b1 = d_sum(rng);
+    int a2 = d_sum(rng);
+    int b2 = d_sum(rng);
+    haexa::Hex<int> h1(a1, b1, -(a1 + b1));
+    haexa::Hex<int> h2(a2, b2, -(a2 + b2));
+    cout << h1 << " + " << h2 << " = " << h1 + h2 << '\t' << ((h1 + h2).valid() ? "ok" : "invalid") << endl;
+  }
+
+  cout << "\nHex subtraction" << endl;
+  for (int i = 0; i < 5; ++i) {
+    int a1 = d_sum(rng);
+    int b1 = d_sum(rng);
+    int a2 = d_sum(rng);
+    int b2 = d_sum(rng);
+    haexa::Hex<int> h1(a1, b1, -(a1 + b1));
+    haexa::Hex<int> h2(a2, b2, -(a2 + b2));
+    cout << h1 << " + " << h2 << " = " << h1 - h2 << '\t' << ((h1 - h2).valid() ? "ok" : "invalid") << endl;
+  }
+
+  cout << "\nHex scalar multiplication" << endl;
+  cout.precision(2);
+  for (int i = 0; i < 10; ++i) {
+    // generate some kinda pseudo-sensible decimal hex
+    int a = d_sum(rng);
+    int b = d_sum(rng);
+    double z = d_rounding(rng) * 3;
+    haexa::Hex<int> hex(a, b, -(a + b));
+    cout << hex << " * " << z << " = " << hex*z << " = "
+         << (hex*z).round() << '\t' << ((hex*z).round().valid() ? "ok" : "invalid") << endl;
+  }
+
+  cout << "\nHex scalar division" << endl;
+  cout.precision(2);
+  for (int i = 0; i < 10; ++i) {
+    // generate some kinda pseudo-sensible decimal hex
+    int a = d_sum(rng);
+    int b = d_sum(rng);
+    double z = d_rounding(rng) * 3;
+    haexa::Hex<int> hex(a, b, -(a + b));
+    cout << hex << " / " << z << " = " << hex*z << " = "
+         << (hex/z).round() << '\t' << ((hex/z).round().valid() ? "ok" : "invalid") << endl;
+  }
 }
